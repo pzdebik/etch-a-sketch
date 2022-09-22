2a36ca979b708a33df01f9df6263d42da57e7b3f
Patryk <patrykzdebik@gmail.com>
2022-09-22 14:59:12 +0200

    Add mouseover event listener to all items
    Added mouseover listener to all divs with
    .item class. When you hover over the grid,
    you change the div background color.

--- main.js
+++ main.js
@@ -3,6 +3,18 @@ const container = document.querySelector('#container');
 for (let i = 0; i <= 32; i++){
     const newDiv = document.createElement('div');
     newDiv.classList.add('item');
+    newDiv.style.height = '20px';
+    newDiv.style.width = '20px';
     container.appendChild(newDiv);
 }
+
+const items = document.querySelectorAll('.item');
+
+//When you hover over the grid, you change the div background color
+items.forEach((item) => {
+    item.addEventListener('mouseover', () => {
+        item.style.backgroundColor = 'black';
+    });
+    
+});
 
