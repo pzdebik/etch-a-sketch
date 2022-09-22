57fce6d5bb84d69a10337609e66d5ece4afd1362
Patryk <patrykzdebik@gmail.com>
2022-09-22 16:22:16 +0200

    Add to makeGrid func if else statement

--- main.js
+++ main.js
@@ -5,23 +5,24 @@ const button = document.createElement('button');
 button.textContent = 'Generate your canvas!';
 body.appendChild(button);
 
-let gridSize = 0;
-
 button.addEventListener('click', () => {
-    gridSize = +prompt('Enter the amount of squares per side: ');
+    makeGrid(+prompt('Enter the amount of squares per side: '));
 })
 
-console.log(gridSize);
 
 function makeGrid(size) {
-    for (let i = 1; i <= size; i++){
-        const newDiv = document.createElement('div');
-        newDiv.classList.add('item');
-        newDiv.style.height = '10px';
-        newDiv.style.width = '10px';
-        container.appendChild(newDiv);
-    }
 
+    if (size > 100) {
+        alert('Too high, my brother. Try less than 100.');
+    } else {
+        for (let i = 1; i <= size; i++){
+            const newDiv = document.createElement('div');
+            newDiv.classList.add('item');
+            newDiv.style.height = '10px';
+            newDiv.style.width = '10px';
+            container.appendChild(newDiv);
+        }
+    }
 }
 
 
